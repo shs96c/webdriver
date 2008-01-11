67a10c5aa6c2eb6269f9aed8d631ca46740cbb9b
simon.m.stewart <simon.m.stewart>
2008-01-11 22:51:58 +0000

    SimonStewart: Example of how the OO API might look

--- jobbie/experiment.py
+++ jobbie/experiment.py
@@ -1,8 +1,16 @@
 from ctypes import *
 
-l = windll.InternetExplorerDriver
+class IeDriver:
+  def __init__(self):
+    self.driverLib = windll.InternetExplorerDriver
+    self.driver = self.driverLib.webdriver_newDriverInstance()
+    
+  def get(self, url):
+    self.driverLib.webdriver_get(self.driver, url)
 
-driver = l.webdriver_newDriverInstance()
-
-l.webdriver_get(driver, u"http://www.google.com");
-l.webdriver_deleteDriverInstance(driver)
+  def __del__(self):
+    self.driverLib.webdriver_deleteDriverInstance(self.driver)
+    
+    
+driver = IeDriver()
+driver.get(u"http://www.google.com")
