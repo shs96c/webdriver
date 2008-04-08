d606b05321d45d0a95cd9877a377bbcbd2fe514e
simon.m.stewart <simon.m.stewart>
2008-04-08 11:34:12 +0000

    SimonStewart: Creating a branch to experiment with the .net bindings from revision 310.

---
+++ jobbie/src/csharp/ManagedInternetExplorerDriver/InternetExplorerDriver.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+using IWebDriver = com.googlecode.webdriver.IWebDriver;
+
+namespace com.googlecode.webdriver.ie
+{
+    public class InternetExplorerDriver : IWebDriver
+    {
+        [DllImport("InternetExplorerDriver")]
+        private static extern IntPtr webdriver_newDriverInstance();
+        public InternetExplorerDriver()
+        {
+            handle = webdriver_newDriverInstance();
+        }
+
+
+        [DllImport("InternetExplorerDriver")]
+        private static extern void webdriver_deleteDriverInstance(IntPtr handle);
+        ~InternetExplorerDriver()
+        {
+            Close();
+            webdriver_deleteDriverInstance(handle);
+        }
+
+        [DllImport("InternetExplorerDriver", CharSet = CharSet.Unicode)]
+        private static extern void webdriver_get(IntPtr handle, string url);
+        public void Get(string url)
+        {
+            webdriver_get(handle, url);
+        }
+
+        [DllImport("InternetExplorerDriver", CharSet = CharSet.Unicode)]
+        private static extern string webdriver_getCurrentUrl(IntPtr handle);
+        public string CurrentUrl
+        {
+            get
+            {
+                Console.WriteLine("Getting current url: " + handle);
+                return webdriver_getCurrentUrl(handle);
+            }
+        }
+
+        [DllImport("InternetExplorerDriver")]
+        private static extern void webdriver_close(IntPtr driver);
+        public void Close()
+        {
+            webdriver_close(handle);
+        }
+
+        private IntPtr handle;
+    }
+}
--- jobbie/src/csharp/ManagedInternetExplorerDriver/Properties/AssemblyInfo.cs
+++ jobbie/src/csharp/ManagedInternetExplorerDriver/Properties/AssemblyInfo.cs
@@ -1,24 +1,26 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
 // General Information about an assembly is controlled through the following 
-// set of attributes. Change these GetAttribute values to modify the information
+// set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly : AssemblyTitle("WebDriver")]
-[assembly : AssemblyDescription("")]
-[assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("Thoughtworks")]
-[assembly : AssemblyProduct("WebDriver")]
-[assembly: AssemblyCopyright("Copyright © Thoughtworks 2007")]
-[assembly : AssemblyTrademark("")]
-[assembly : AssemblyCulture("")]
+[assembly: AssemblyTitle("ManagedInternetExplorerDriver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Google")]
+[assembly: AssemblyProduct("ManagedInternetExplorerDriver")]
+[assembly: AssemblyCopyright("Copyright © Google 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible GetAttribute to true on that type.
-[assembly: ComVisible(true)]
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly : Guid("adbf8cdb-6d85-48cb-8d8b-64f688d6596a")]
+[assembly: Guid("dbf78558-80d6-452d-9462-2a007e414e11")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly : AssemblyVersion("1.0.0.0")]
-[assembly : AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
