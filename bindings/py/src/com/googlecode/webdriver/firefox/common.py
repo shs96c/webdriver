864d0dccc1c762c86eb9ad73e3884964da5f8e7c
Jiayao.Yu <Jiayao.Yu>
2008-07-04 15:58:26 +0000

    JiayaoYu:python binding:remove the old directory structure

--- bindings/py/src/com/googlecode/webdriver/firefox/common.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logging.basicConfig()
-logger = logging.getLogger("webdriver")
-logger.setLevel(logging.DEBUG)
-HTML_ROOT = "../../../common/src/web/"
--- bindings/py/src/com/googlecode/webdriver/firefox/exceptions.py
+++
@@ -1,7 +0,0 @@
-class ErrorInResponseException(Exception):
-  def __init__(self, *args):
-    Exception.__init__(self, *args)
-
-class InvalidSwitchToTargetException(Exception):
-  def __init__(self, *args):
-    Exception.__init__(self, *args)
