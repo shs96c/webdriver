de35d07b19c23d63e8e9febc62bab4774403f218
Jiayao.Yu <Jiayao.Yu>
2008-09-06 01:06:01 +0000

    JiayaoYu: Make python binding work on windows

--- bindings/py/src/webdriver/firefox/firefoxlauncher.py
+++ bindings/py/src/webdriver/firefox/firefoxlauncher.py
@@ -2,12 +2,20 @@ from subprocess import Popen
 from subprocess import PIPE
 import time
 import platform
+import os
 
 class FirefoxLauncher(object):
   
   def LaunchBrowser(self):
     if platform.system() == "Darwin":
       cmd = "/Applications/Firefox.app/Contents/MacOS/firefox"
+    elif platform.system() == "Windows":
+      program_files = os.getenv("PROGRAMFILES")
+      if program_files is None:
+        program_files = "\\Program Files"
+      import pdb
+      pdb.set_trace()
+      cmd = os.path.join(program_files, "Mozilla Firefox\\firefox.exe") 
     else:
       cmd = Popen(["which", "firefox2"], stdout=PIPE).communicate()[0].strip()
     self.process = Popen([cmd, "-P", "WebDriver"])
---
+++ py_test.bat
@@ -0,0 +1,6 @@
+cd bindings\py\src
+
+set PYTHONPATH=%PYTHONPATH%;.;..\lib 
+
+python webdriver/unittests/basic_tests.py
+cd ..\..\..
